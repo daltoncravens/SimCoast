3d4b0886d7ff1b6e1b24a067e36345b0d9e4724c
Jose Lopez <70908944+jlopez7693@users.noreply.github.com>
2021-12-02 14:07:19 -0500

    Added toolbar to start_map.tscn

--- HUD.gd
+++ HUD.gd
@@ -10,7 +10,7 @@ func update_mouse(pos):
 	$StatusBar/MousePosition.text = "Mouse Position: (%s, %s)" % [pos.x, pos.y]
 
 func update_time(date):
-	$Date.text = "Date: %s" % "{month}-{day}-{year}".format ({
+	$Toolbar/Time/VBoxContainer/Date.text = "%s" % "{month}-{day}-{year}".format ({
 		month = str(date.month).pad_zeros(2),
 		day = str(date.day).pad_zeros(2),
 		year = str(date.year).pad_zeros(2)
