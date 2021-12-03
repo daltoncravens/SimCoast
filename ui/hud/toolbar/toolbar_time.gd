3db7daa3bab711810102bc1527c002d3252fed6c
Jose Lopez <70908944+jlopez7693@users.noreply.github.com>
2021-12-03 12:29:49 -0500

    Time controls now function as intended
    Todo: Add slow button

--- ui/hud/toolbar/toolbar_time.gd
+++ ui/hud/toolbar/toolbar_time.gd
@@ -3,4 +3,19 @@ extends HBoxContainer
 export(ButtonGroup) var group
 
 func _ready():
-	print(group.get_buttons())
+	for i in group.get_buttons():
+		i.connect("pressed", self, "button_pressed")
+
+func button_pressed():
+	match group.get_pressed_button().get_name():
+		'Pause_Button':
+			Global.gamePaused = true
+			print("Pausing game")
+		'Play_Button':
+			Global.gamePaused = false
+			Global.gameSpeed = 20000
+			print("Game Speed: Normal")
+		'Fast_Forward_Button':
+			Global.gamePaused = false
+			Global.gameSpeed = 60000
+			print("Game Speed: Fast")
