dcb7e3662e74d5cdfe9ac0e6873e78af9b2c6230
Jose Lopez <70908944+jlopez7693@users.noreply.github.com>
2022-03-29 17:44:22 -0400

    Added functionality to economy.gd script

--- AI/economy/economy.gd
+++ AI/economy/economy.gd
@@ -1,24 +1,71 @@
 extends Node
 
-var mapHeight
-var mapWidth
+
+# Need to test to see if this AI is appropriate as a singleton or a child node
 
 
+# Sets up primary blackboard values
 func _ready():
-	mapHeight = Global.mapHeight
-	mapWidth = Global.mapWidth
-	update_queue()
+	$Blackboard.set_data("queue", [])
+	$Blackboard.set_data("queue_empty", true)
 
 
+# Checks if the AI's tile queue is empty
 func _process(delta):
-	# Because the map is scalable, update map dimensions
-	#$Label.text = "Hello script world!"
-	#var msg = $Label.text + " How's it going?"
-	#$Label.text = msg
-	pass
-
-
-func update_queue():
-	var latest = [1,2,3,4,5,6,7,8,9]
-	$Blackboard.set_data("queue", latest)
+	if $Blackboard.get_data("queue_empty") == true:
+		update_AI()
 	
+
+# Obtains a new tile queue and sets it in the blackboard	
+func update_AI():
+	var latest = update_queue()
+	$Blackboard.set_data("queue", latest)
+	$Blackboard.set_data("queue_empty", false)
+	
+
+# Refills the zone queue
+func update_queue() -> Array:
+	# Because the map is scalable, update map dimensions
+	var mapHeight = Global.mapHeight
+	var mapWidth = Global.mapWidth
+	var latest = []
+	for i in mapHeight:
+		for j in mapHeight:
+			var current = Global.tileMap[i][j]
+			var accept		
+			# Check tile zone type. If it's zoned, accept the tile into the queue
+			match(current.zone):
+				Tile.TileZone.LIGHT_RESIDENTIAL:
+					accept = true
+				Tile.TileZone.HEAVY_RESIDENTIAL:
+					accept = true
+				Tile.TileZone.LIGHT_COMMERCIAL:
+					accept = true
+				Tile.TileZone.HEAVY_COMMERCIAL:
+					accept = true
+				_:
+					accept = false	
+			# Check tile infrastructure if not zoned (roads & powerplants)
+			if accept != true:
+				match(current.inf):
+					Tile.TileInf.ROAD:
+						accept = true
+					Tile.TileInf.PARK:
+						accept = true
+					Tile.TileInf.POWER_PLANT:
+						accept = true
+					_:
+						accept = false
+			if accept:
+				latest.push_back(current)
+	return latest
+
+
+
+
+
+
+
+
+
+
