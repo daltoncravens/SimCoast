149130e5d22273d393bdf20e9f3fc9773340658e
Luan Tran <ltran3@ufl.edu>
2022-03-30 13:32:25 -0400

    Tax Loop Update

--- scripts/Econ.gd
+++ scripts/Econ.gd
@@ -18,12 +18,27 @@ const REMOVE_COMMERCIAL_BUILDING  = 0.3
 var money = 100000
 var city_tax_rate = BASE_TAX_RATE
 var city_income = 0 # Net Profit
+var property_tax_rate = 0.01 
 
 # adjustVal parameter takes in the exact amount loss or gain towards the player money 
 # EX: if player spends $500 then adjustVal should be -500
 func adjust_player_money(adjustVal):
 	money += adjustVal
 	$Money.text = "$" + comma_values(str(money))
+	
+func collectTaxes():
+	var taxProfit = 0
+	var mapHeight = Global.mapHeight
+	var mapWidth = Global.mapWidth
+	for i in mapHeight:
+		for j in mapWidth:
+			var currTile = Global.tileMap[i][j]
+			if currTile.zone == Tile.TileZone.HEAVY_COMMERCIAL || currTile.zone == Tile.TileZone.LIGHT_COMMERCIAL:
+				taxProfit += currTile.profitRate * (city_tax_rate + property_tax_rate)
+			elif currTile.zone == Tile.TileZone.HEAVY_RESIDENTIAL || currTile.zone == Tile.TileZone.LIGHT_RESIDENTIAL:
+				taxProfit += currTile.profitRate * property_tax_rate
+	adjust_player_money(taxProfit)
+			
 	
 
 # Helper Functions
