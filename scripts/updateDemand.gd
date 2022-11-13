a26af394e37e45d2312879980c09659e75275f1b
Andrew <ahlopez1999@gmail.com>
2022-11-13 16:35:58 -0500

    Completed demand and HUD
    First implementation of demand HUD. Changed residential demand to account for available move-in space.

--- scripts/updateDemand.gd
+++ scripts/updateDemand.gd
@@ -9,6 +9,8 @@ func calcResidentialDemand():
 	var mapHeight = Global.mapHeight
 	var mapWidth = Global.mapWidth
 	
+	var maxpop = 0
+	var currpop = 0
 	var resDemand = 0
 	var resZones = 0
 	var avgleave = 0
@@ -18,15 +20,22 @@ func calcResidentialDemand():
 			var currTile = Global.tileMap[i][j]
 			if currTile.zone == Tile.TileZone.LIGHT_RESIDENTIAL || currTile.zone == Tile.TileZone.HEAVY_RESIDENTIAL:
 				# calc chance of pop moving in (based on updatePopulation chance)
-				avgmove += (currTile.happiness + currTile.landvalue)
+				avgmove += (currTile.happiness)
 				# calc of pop leaving (based on updatePopulation chance)
 				if !currTile.is_powered():
 					avgleave += NO_POWER_UNHAPPINESS
-				elif currTile.get_status() == Tile.TileStatus.LIGHT_DAMAGE || currTile.get_status() == Tile.TileStatus.MEDIUM_DAMAGE:
+				if currTile.get_status() == Tile.TileStatus.LIGHT_DAMAGE || currTile.get_status() == Tile.TileStatus.MEDIUM_DAMAGE:
 					avgleave += DAMAGE_UNHAPPINESS
 				elif currTile.get_status() == Tile.TileStatus.HEAVY_DAMAGE:
 					avgleave += SEVERE_DAMAGE_UNHAPPINESS
+				if currTile.zone == Tile.TileZone.LIGHT_RESIDENTIAL:
+					maxpop += 16
+				else:
+					maxpop += 16 #TODO may need to be changed if heavy residential zoning is changed
+				currpop += currTile.data[2]
 				resZones += 1
+	if avgleave == 0 && avgmove == 0:
+		return 0
 	avgleave /= resZones
 	avgmove /= resZones
 	# if more citizens are leaving than moving in, no demand
@@ -34,8 +43,9 @@ func calcResidentialDemand():
 		resDemand = 0
 	# otherwise scale demand based on the difference between the two
 	else:
-		resDemand = (avgmove - avgleave)/10
-	#print("Residential Demand: " + resDemand)
+		resDemand = round((avgmove - avgleave)/20)
+		# reduce demand if there is available space
+		resDemand -= (maxpop - currpop)/10
 	return resDemand
 
 func calcCommercialDemand():
@@ -63,5 +73,12 @@ func calcCommercialDemand():
 	# Don't accept negative demand
 	if comDemand < 0:
 		comDemand = 0;
-	#print("Commercial Demand: " + comDemand)
 	return comDemand
+	
+func get_demand():
+	var resDemand = 0
+	var comDemand = 0
+	resDemand = calcResidentialDemand()
+	comDemand = calcCommercialDemand()
+	get_node("/root/CityMap/HUD/TopBar/HBoxContainer/Demand").text = "Residential Demand: " + str(resDemand) + "/10" + " Commercial Demand: " + str(comDemand) + "/10"
+	return
